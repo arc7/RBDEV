93df78faa2501b3ce5f3c960e40999679f6ec8b2
arc7 <mimoun.fe@voila.fr>
2014-03-12 21:18:48 +0100

    dd

--- js/main.js
+++ js/main.js
@@ -12,15 +12,27 @@
     // onSuccess: Afficher le nom de tous les contacts
     //
     function onSuccess(contacts) {
+		var elements = document.getElementById('contacts');
         for (var i=0; i<contacts.length; i++) {
-            alert("Nom complet : " + contacts[i].name.formatted + "\n" + 
-                    "Nom de famille : "  + contacts[i].name.familyName + "\n" + 
-                    "Prénom : "  + contacts[i].name.givenName);
-        }
-    };
-
+		elements.innerHTML = elements.innerHTML + ' ID : ' + contacts[i].id + '<br>';
+		elements.innerHTML = elements.innerHTML + 'Name : ' + contacts[i].name.formatted +'<br>';
+				for(var j=0; j<contacts[i].phoneNumbers.length; j++) {
+						if((contacts[i].phoneNumbers[j]!=null)&&(contacts[i].phoneNumbers[j]!='undefined')) {
+							elements.innerHTML = elements.innerHTML + 'Phone : ' + contacts[i].phoneNumbers[j].value +'<br>';
+						}
+					}
+				for(var j=0; j<contacts[i].emails.length; j++) {
+						if(contacts[i].emails[j]!=null) {
+							elements.innerHTML = elements.innerHTML + 'Email : ' + contacts[i].emails[j].value + '<br>';
+						}
+					}
+		}
+	};
     // onError: Echec de récupération des contacts
     //
     function onError(contactError) {
         alert('onError!');
     }
+	var deviceInfo = function() {
+        document.getElementById("uuid").innerHTML = device.uuid;
+	};
