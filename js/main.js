f10fc5e295b713da77592b98118bf049f89d4650
arc7 <mimoun.fe@voila.fr>
2014-03-12 20:56:56 +0100

    sss

--- js/main.js
+++ js/main.js
@@ -3,25 +3,24 @@
     // PhoneGap est prêt
     //
     function onDeviceReady() {
-        // Rechercher tous les contacts qui ont 'Bob' dans l'un de leurs champs de nom
         var options = new ContactFindOptions();
-        options.filter=""; 
-        var fields = ["displayName", "name"];
-        navigator.contacts.find(fields, onSuccess, onError, options);
+        options.filter="";
+        filter = ["displayName","name"];
+        navigator.contacts.find(filter, onSuccess, onError, options);
     }
 
-    // onSuccess: Afficher le nom de tous les contacts trouvés
+    // onSuccess: Afficher le nom de tous les contacts
     //
     function onSuccess(contacts) {
         for (var i=0; i<contacts.length; i++) {
-            alert("Nom  = " + contacts[i].displayName);
+            alert("Nom complet : " + contacts[i].name.formatted + "\n" + 
+                    "Nom de famille : "  + contacts[i].name.familyName + "\n" + 
+                    "Prénom : "  + contacts[i].name.givenName; 
         }
-    }
+    };
 
     // onError: Echec de récupération des contacts
     //
     function onError(contactError) {
         alert('onError!');
     }
-
-   
