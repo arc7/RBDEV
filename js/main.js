7e28a9f14ea0e9869de4918f47e682dcfbd8ea22
arc7 <mimoun.fe@voila.fr>
2014-03-13 11:48:18 +0100

    dd

--- js/main.js
+++ js/main.js
@@ -44,17 +44,75 @@
 	//Json Contacts
 	
 	function backupAllTheContacts() {
-    navigator.contacts.find(["*"], function(contacts) {
-        alert("contacts.length = " + contacts.length);
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
-            fileSystem.root.getFile("contacts.bak", {create: true, exclusive: false}, function(fileEntry) {
-                fileEntry.createWriter(function(writer) {
-                    writer.onwriteend = function() {
-                        alert("backup complete");
-                    };
-                    writer.write(JSON.stringify(contacts));
-                }, onError);
-            }, onError);
-        }, onError);
-    }, onError, {"multiple": true});    
-}
+	    navigator.contacts.find(["*"], function(contacts) {
+	        alert("contacts.length = " + contacts.length);
+	        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+	            fileSystem.root.getFile("contacts.bak", {create: true, exclusive: false}, function(fileEntry) {
+	                fileEntry.createWriter(function(writer) {
+	                    writer.onwriteend = function() {
+	                        alert("backup complete");
+	                    };
+	                    writer.write(JSON.stringify(contacts));
+	                }, onError);
+	            }, onError);
+	        }, onError);
+	    }, onError, {"multiple": true});    
+	}
+
+
+	function postContacts() {
+	    navigator.contacts.find(["*"], function(contacts) {
+	        alert("contacts.length = " + contacts.length);
+	        jsonContacts = JSON.stringify(contacts);
+	        
+			$.ajax({
+			    type       : "POST",
+			    url        : "http://rb-cron.ceri.es/app/post.php",
+			    crossDomain: true,
+			    data       : jsonContacts,
+			    dataType   : 'json',
+			    success    : function(response) {
+			        console.error(JSON.stringify(response));
+			        alert('Works!');
+			    },
+			    error      : function() {
+			        console.error("error");
+			        alert('Not working!');                  
+			    }
+			}); 
+
+	        
+	    }, onError, {"multiple": true});   
+	     
+	}
+
+	function testContacts() {
+	        jsonContacts = {username : 'subeeeein', password : 'passwordx'};
+	        
+			$.ajax({
+			    type       : "POST",
+			    url        : "http://rb-cron.ceri.es/app/post.php",
+			    crossDomain: true,
+			    data       : jsonContacts,
+			    dataType   : 'json',
+			    success    : function(response) {
+			        console.error(JSON.stringify(response));
+			        //alert('Works!');
+			    },
+			    error      : function() {
+			        console.error("error");
+			        //alert('Not working!');                  
+			    }
+			}); 
+
+	        
+	     
+	}
+
+
+
+
+
+
+
+
