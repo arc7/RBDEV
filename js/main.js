05e35aa47fbc92dc4b162d7dbf27ec66781b7e0f
vidament-brou <adley.vidament-brou.etu@univ-lemans.fr>
2014-03-20 10:56:17 +0100

    test

--- js/main.js
+++ js/main.js
@@ -26,8 +26,8 @@ function postJson(action, obj, callback, needUserData) {
 	
 	console.log(obj);
 	
-    //jsonTosend = "data="+JSON.stringify(obj);
-    jsonTosend = JSON.stringify(obj);
+    jsonTosend = "data="+JSON.stringify(obj);
+    //jsonTosend = JSON.stringify(obj);
     alert(jsonTosend);
     console.log("http://rb.cerivan.com/app/call/post.php?g=yes&"+jsonTosend);
     
