83823367d76f0037528e277d647951dac3837ebd
vidament-brou <adley.vidament-brou.etu@univ-lemans.fr>
2014-03-20 11:20:35 +0100

    test

--- js/main.js
+++ js/main.js
@@ -36,9 +36,10 @@ function postJson(action, obj, callback, needUserData) {
 	    type       : "POST",
 	    url        : "http://rb.cerivan.com/app/call/post.php",
 	    data       : jsonTosend,
+	    dataType : 'json',
 	    success    : function(response) {
 	    	if (response.success == false) {
-		    	alert("Operation echoue !"+response.success)
+		    	alert("Operation echoue ! : "+response.success)
 			jsonError(response);
 		    	return false;
 	    	}
@@ -59,6 +60,7 @@ function postJson(action, obj, callback, needUserData) {
 	        return response;
 	    },
 	    error      : function() {
+		    alert(this.url);
 		    jsonError(false);
 	    }
 	}); 
--- js/user.js
+++ js/user.js
@@ -47,85 +47,8 @@ function getPhoneNumber() {
 	    }, onError, {"multiple": true});   
 	     
 	} 
-
-
-
-    //
-    function getContacts() {
-        var options = new ContactFindOptions();
-        options.filter="";
-		options.multiple=true;
-        filter = ["displayName","name", "phoneNumbers", "emails"];
-		navigator.contacts.find(filter, onSuccess, onError, options);
-    }
-
-
-	function onSuccess(contacts) {
-		var elements =  new Object();
-		elements.nombreContacts= contacts.length ;
-        for (var i=0; i<contacts.length; i++) {
-		elements.id =  contacts[i].id ;
-		elements.name = contacts[i].name.givenName;
-				for(var j=0; j<contacts[i].phoneNumbers.length; j++) {
-						if((contacts[i].phoneNumbers[j]!=null)&&(contacts[i].phoneNumbers[j]!='undefined')) {
-							elements.phoneNumbers =  contacts[i].phoneNumbers[j].value ;
-						}
-					}
-				for(var j=0; j<contacts[i].emails.length; j++) {
-						if(contacts[i].emails[j]!=null) {
-							elements.emails =  contacts[i].emails[j].value;
-						}
-					}
-		}
-		elements.uuid = device.uuid;
-		jsonContacts = "data=" + JSON.stringify(elements);
-	        alert(jsonContacts);
-	        
-			$.ajax({
-			    type       : "POST",
-			    url        : "http://rb.cerivan.com/app/post.php",
-			    data       : jsonContacts,
-			    dataType   : 'json',
-			    success    : function(response) {
-			        console.log(JSON.stringify(response));
-			        console.log('Works!');
-			    },
-			    error      : function() {
-			        console.error("Erreur");
-			        console.log('Not working!');                  
-			    }
-			}); 
-
-	};
-
-    // onError: Echec de rÃ©cupÃ©ration des contacts
-    //
-    function onError(contactError) {
-        alert('onError!');
-    }
-
-    
-	var deviceinfo = function() {   
-		document.getElementById("uuid").innerHTML = device.uuid;
-		};
 		
-	//Json Contacts
 	
-	function backupAllTheContacts() {
-	    navigator.contacts.find(["*"], function(contacts) {
-	        alert("contacts.length = " + contacts.length);
-	        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
-	            fileSystem.root.getFile("contacts.bak", {create: true, exclusive: false}, function(fileEntry) {
-	                fileEntry.createWriter(function(writer) {
-	                    writer.onwriteend = function() {
-	                        alert("backup complete");
-	                    };
-	                    writer.write(JSON.stringify(contacts));
-	                }, onError);
-	            }, onError);
-	        }, onError);
-	    }, onError, {"multiple": true});    
-	}
 
 
 //#######################################################################################
